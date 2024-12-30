ed4523289484ccb8830739b5da2ac1b18fd66382
suleman37 <sulemanshakeel37@gmail.com>
2024-12-30 14:26:47 +0500

    adding Atlas Mongo DB

--- Config/DB.js
+++ Config/DB.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/CRM_Portal", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
